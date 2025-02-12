27c59ba890203e0cbe4cc3c54cd91bce5faeb970
Krunal Shambharkar <krunalss@outlook.com>
2025-02-12 16:10:59 +0530

    changed order of import in app py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from langchain_groq import ChatGroq 
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -10,6 +9,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 import time
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq 
 load_dotenv()
 
 #load groq env varibale
